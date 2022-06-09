3f42c38b1144aed9fe3f46e99a7850c893d008f2
NAIT <usmanbelloa@gmail.com>
2022-06-09 12:18:18 -0400

    gpt3 half

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog} from './blog/Blog'
+export {default as Features} from './features/Feature'
+export {default as Footer} from './footer/Footer'
+export {default as Header} from './header/Header'
+export {default as Possibility} from './possibilty/Possibility'
+export {default as Whatgpt3} from './whatGPT3/Whatgpt3'
